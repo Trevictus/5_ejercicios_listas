3d5ed9d7606cf31e0b91e553b6020e4bab9bf38e
vgomtej353 <gtvictor@iesrafaelalberti.es>
2024-11-14 16:02:05 +0100

    Ejercicio 5. TODO pytests

--- src/P3.1_ej5.py
+++ src/P3.1_ej5.py
@@ -1,13 +1,21 @@
 # Ejercicio 3.1.5¶
 """Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas."""
+def almacenar_lista():
+    lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    return lista
 
+def mostrar_lista_inversa(lista):
+    reves = ", ".join(map(str, lista[::-1]))
+    return reves
+    
 
 
 
 
 
 def main():
-    pass
+    lista = almacenar_lista()
+    print(mostrar_lista_inversa(lista))
 
 
 if __name__ == "__main__":
